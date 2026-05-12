392feafe29ea38c2514a9f6595024464c4718e14
JackensonCharles <jcharles@trimergecpa.com>
2026-05-12 10:26:52 -0400

    feat: update font to Inter to match teammate's styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sharedComponents/Sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-Inter",
   subsets: ["latin"],
 });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} h-full antialiased`}>
+    <html lang="en" className={`${inter.variable} h-full antialiased`}>
       <body className="flex flex-row h-screen overflow-hidden bg-slate-50">
 
         {/* Sidebar — client component, manages its own state */}
