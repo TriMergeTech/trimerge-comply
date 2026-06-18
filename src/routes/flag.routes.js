22eca16ef5a9881810c87d47066158dfe3393f48
JackensonCharles <jcharles@trimergecpa.com>
2026-06-17 22:41:50 -0400

    delete src in frontend branch

--- src/routes/flag.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { protect } = require('../middleware/auth.middleware');
-const { getFlags, getFlagById } = require('../controllers/flag.controller');
-
-router.get('/', protect, getFlags);
-router.get('/:id', protect, getFlagById);
-
-module.exports = router;
